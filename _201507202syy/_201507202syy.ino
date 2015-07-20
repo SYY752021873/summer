8828bcc62093104d17a02469ddc3fb84d93a6a75
SYY752021873 <syy752021873@gmail.com>
2015-07-20 15:45:05 +0800

    commit

---
+++ _201507202syy/_201507202syy.ino
@@ -0,0 +1,51 @@
+int x=25;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  
+ 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ digitalWrite(2,HIGH);
+   delay(x);
+   digitalWrite(2,LOW);
+      delay(x);
+  digitalWrite(3,HIGH);
+   delay(x);
+   digitalWrite(3,LOW);
+      delay(x);
+ digitalWrite(4,HIGH);
+   delay(x);
+   digitalWrite(4,LOW);
+      delay(x);
+  digitalWrite(5,HIGH);
+   delay(x);
+   digitalWrite(5,LOW);
+      delay(x);
+  digitalWrite(6,HIGH);
+   delay(x);
+   digitalWrite(6,LOW);
+      delay(x);
+  digitalWrite(7,HIGH);
+   delay(x);
+   digitalWrite(7,LOW);
+      delay(x);
+ digitalWrite(8,HIGH);
+   delay(x);
+   digitalWrite(8,LOW);
+      delay(x);
+  digitalWrite(9,HIGH);
+   delay(x);
+   digitalWrite(9,LOW);
+      delay(x);     
+}
+
